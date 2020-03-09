40d8cd8fc47e7305c1c522d4ae525f84f9276c2d
Oscar <oscar.becerril@servexternos.gruposantander.com>
2020-03-09 14:16:36 +0100

    Update compartida.bat
    Actualizada clave

--- compartida.bat
+++ compartida.bat
@@ -1,4 +1,4 @@
 @echo off
 
-net use z: \\Vmnglucodp01.ngl.corp\implantacion Krak0via49 /user:NGL\admx099081 /persistent:yes
+net use z: \\Vmnglucodp01.ngl.corp\implantacion Krak0via50 /user:NGL\admx099081 /persistent:yes
 
